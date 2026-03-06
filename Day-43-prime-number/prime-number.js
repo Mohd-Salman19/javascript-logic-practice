22c74c7f69500f172dac01b5c687f6e6c92230ee
mohammad salman <mohdsalman25555@gmail.com>
2026-03-06 23:56:15 +0530

    Add prime number checker function

---
+++ Day-43-prime-number/prime-number.js
@@ -0,0 +1,24 @@
+// Day 43: Check Prime Number
+
+function isPrime(num) {
+
+    if (num <= 1) {
+        return false;
+    }
+
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+let number = 7;
+
+if (isPrime(number)) {
+    console.log(number + " is a Prime Number");
+} else {
+    console.log(number + " is NOT a Prime Number");
+}
